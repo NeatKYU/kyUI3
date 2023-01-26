9296d9dc233ef59981b86a13f1819437599e9ce0
jeff.park <jeff.park@machbase.com>
2023-01-26 18:07:32 +0900

    🎉 프로젝트 시작

---
+++ sandbox/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <main>
+      <h1>Welcome to your your sandbox environment</h1>
+      <Input
+        placeholder="입력하세요"
+        type="email"
+      />
+    </main>
+</template>
+  
+<script setup lang="ts">
+import Input from "../src/Input.vue";
+</script>
+
+<style lang="scss" scoped></style>
