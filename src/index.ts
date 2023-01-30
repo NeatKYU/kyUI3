eb010173b369336ecc3cc9e2135fd9e2dc4664d9
jeff.park <jeff.park@machbase.com>
2023-01-30 09:42:04 +0900

    🐛 컴포넌트 등록안되는 문제 테스트

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,19 @@ import './fontawesome/index';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 const components = [
-    Input,
-    Icon,
+    {
+        name: 'c-input',
+        value: Input,
+    }, {
+        name: 'c-icon',
+        value: Icon
+    }
 ]
 
 const kyui3 = {
     install(app: App) {
         components.map((item) => {
-            app.component(item.name, item)
+            app.component(item.name, item.value)
         })
         app.component('f-icon', FontAwesomeIcon)
     }
