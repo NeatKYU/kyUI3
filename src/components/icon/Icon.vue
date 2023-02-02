0e96a121e3cab2e55e50020d871b526d2715485b
jeff.park <jeff.park@machbase.com>
2023-02-02 16:13:25 +0900

    💄 아이콘 사이즈 추가

--- src/components/icon/Icon.vue
+++ src/components/icon/Icon.vue
@@ -1,18 +1,45 @@
 <template>
-    <span class="icon" :class="[props.size]">
+    <span class="icon" :class="[classes]">
         <f-icon :icon="props.icon" />
     </span>
 </template>
 
 <script setup lang="ts" name="c-icon">
-import { } from 'vue'
+import { computed } from 'vue'
 
 const props = defineProps({
     pack: String,
     icon: String,
-    size: String,
-    customSize: String,
-    customClass: String,
+    size: {
+        type: String,
+        default: 'medium'
+    },
+})
+
+const classes = computed(() => {
+    return {
+        'is-small': props.size === 'small',
+        'is-medium': props.size === 'medium',
+        'is-large': props.size === 'large',
+    }
 })
 
 </script>
+
+<style lang="scss" scoped>
+.icon {
+    display: flex;
+
+    &.is-small {
+        font-size: 12px;
+    }
+
+    &.is-medium {
+        font-size: 16px;
+    }
+
+    &.is-large {
+        font-size: 22px;
+    }
+}
+</style>
