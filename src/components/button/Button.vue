6707ddf211b8aa1fa18bd66c2921d91c2dc90640
jeff.park <jeff.park@machbase.com>
2023-02-03 11:40:56 +0900

    ✨ 버튼 물결 효과 추가

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -1,9 +1,11 @@
 <template>
     <component
+        ref="cButton"
         :is="computedTag"
         class="c-button"
         :class="[ type, classes ]"
         v-bind="$attrs"
+        @click="animateWave"
     >
         <c-icon v-if="leftIcon && !isIconButton" :icon="leftIcon" />
         <span v-if="label && !isIconButton">{{ label }}</span>
@@ -16,7 +18,7 @@
 </template>
 
 <script setup lang="ts" name="c-button">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import config from '../../utils/config';
 
 /**
@@ -65,6 +67,14 @@ const props = defineProps({
         default: 'star',
     }
 })
+interface waves {
+    x: number;
+    y: number;
+    show: boolean;
+}
+
+const cButton = ref<HTMLDivElement>();
+const waves = ref([] as waves[]);
 
 const computedTag = computed(() => {
     if (props.disabled !== undefined && props.disabled !== false) {
@@ -84,6 +94,42 @@ const classes = computed(() => {
         'is-icon-button': props.isIconButton,
     }
 })
+
+const animateWave = (e: PointerEvent) => {
+    let el = cButton.value;
+    if (el) {
+        let div = document.createElement('div');
+
+        div.style.position = 'absolute';
+        div.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+        div.style.borderRadius = '50%';
+        div.style.width = '100px';
+        div.style.height = '100px';
+        // div.style.transform = 'translate(-50%, -50%)';
+        div.style.animation = 'wave 1s';
+        div.style.opacity = '0';
+
+        // set div animation
+        const waveEffect = [
+            { transform: 'scale(0)', opacity: 1 },
+            { transform: 'scale(1)', opacity: 0 },
+        ]
+        const setTime = {
+            duration: 500,
+            iterations: 1,
+        }
+
+        // https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
+        div.animate(waveEffect, setTime);
+
+        el.appendChild(div);
+
+        // remove div
+        setTimeout(() => {
+            div.remove();
+        }, 1000);
+    }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -92,6 +138,9 @@ const classes = computed(() => {
 .c-button {
     width: auto;
     height: 40px;
+
+    position: relative;
+    overflow: hidden;
 
     display: inline-flex;
     justify-content: center;
@@ -171,4 +220,29 @@ const classes = computed(() => {
         height: 50px;
     }
 }
+
+.wave {
+    position: absolute;
+    border-radius: 50%;
+    background-color: rgba(0, 0, 0, 0.3);
+
+    width: 100px;
+    height: 100px;
+    transform: translate(-50%, -50%);
+
+    animation: wave 1s forwards;
+    opacity: 0;
+}
+
+@keyframes wave {
+	from {
+        opacity: 1;
+        transform: scale(0);
+    }
+    to {
+        opacity: 0;
+        transform: scale(1);
+    }
+}
+
 </style>
