8f37dcc5736a3e3bf46347f0839f28ae7f8d4c0c
jeff.park <jeff.park@machbase.com>
2023-05-16 16:13:36 +0900

    💄 dropdown 기능 추가 및 스타일 변경

--- src/components/dropdown/DropdownItem.vue
+++ src/components/dropdown/DropdownItem.vue
@@ -1,10 +1,20 @@
 <template>
     <div class="c-dropdown-item">
+        <Icon v-if="icon" :icon="icon" class="c-icon" />
         <slot/>
     </div>
 </template>
 
 <script setup lang='ts' name="c-dropdown-item">
+import { defineProps } from 'vue';
+import Icon from '@/components/icon/Icon.vue';
+
+const props = defineProps({
+    icon: {
+        type: String,
+        default: '',
+    }
+})
 </script>
 
 <style lang="scss" scoped>
@@ -13,12 +23,21 @@
     width: 100%;
     height: 30px;
 
-    padding: 5px 10px;
+    padding: 5px;
+
+    display: flex;
+    align-items: center;
+
+    border-radius: $c-border-radius;
     
     cursor: pointer;
 
     &:hover {
         background-color: $c-common-hover-color;
     }
+}
+
+.c-icon {
+    margin-right: 5px;
 }
 </style>
